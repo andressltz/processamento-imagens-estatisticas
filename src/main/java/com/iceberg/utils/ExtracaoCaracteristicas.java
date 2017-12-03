bd407053edc1300c94670db2a6c183705bbb02c1
andresschultz <andreswinck@gmail.com>
2017-12-02 22:55:43 -0200

    calculo de perimetro quadrado

--- src/main/java/com/iceberg/utils/ExtracaoCaracteristicas.java
+++ src/main/java/com/iceberg/utils/ExtracaoCaracteristicas.java
@@ -1,26 +1,39 @@
 package com.iceberg.utils;
 
-import java.awt.Color;
+import javax.swing.JOptionPane;
 
 import com.iceberg.sistema.Imagem;
 
 public class ExtracaoCaracteristicas {
 
 	public static void calcularAreaPerimetroQuadrado(Imagem imagem) {
+		final int whiteColor = -1;
+		int firstPoint = 0;
+		int lastPoint = 0;
+
 		for (int i = 1; i < imagem.getLargura(); i++) {
 			for (int j = 1; j < imagem.getAltura(); j++) {
 				int rgb = imagem.getBufferedImage().getRGB(i, j);
-				Color color = new Color(rgb);
-				if (rgb == -1) { // branco coloca espaço
-					System.out.print("  ");
+				if (rgb == whiteColor) {
+					if (firstPoint != 0) {
+						lastPoint = j;
+					}
 				} else {
-					System.out.print(color.getBlue() + " ");
-					// -16777216
+					if (firstPoint == 0) {
+						firstPoint = j;
+					}
+				}
+
+				if (firstPoint != 0 && lastPoint != 0) {
+					break;
 				}
 			}
-			System.out.println("");
 		}
 
+		int lateral = lastPoint - firstPoint;
+		int perimetro = lateral + lateral + lateral + lateral;
+		int area = lateral * lateral;
+		JOptionPane.showMessageDialog(null, "O perímetro do quadrado é " + perimetro + " pixels e a área é " + area + " pixels. Sua largura é de " + lateral + " pixels.");
 	}
 
 }
